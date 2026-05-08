0c9c93c8dd5c55ebbef2f3b3601b43d2e32368ee
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-05-08 14:38:59 +0200

    return JSON objects from GET and POST endpoints

--- src/main/java/com/example/javaapi/HelloController.java
+++ src/main/java/com/example/javaapi/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.javaapi;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	private String message = "Hello, world!";
+	private List<Map<String, Object>> items = new ArrayList<>();
 
 	@GetMapping("/hello")
-	public String hello() {
-		return message;
+	public List<Map<String, Object>> getItems() {
+		return items;
 	}
 
 	@PostMapping("/hello")
-	public String update(@RequestBody String body) {
-		message = body;
-		return "Updated!";
+	public Map<String, Object> addItem(@RequestBody Map<String, Object> body) {
+		items.add(body);
+		return body;
 	}
 
 }
